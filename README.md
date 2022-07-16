f59e7dc9b1242a19aee31385912b25defb5e6838
April-Hannah-Lena <aprillherwig@gmail.com>
2022-07-16 07:54:03 +0200

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SplitOrderedCollections.jl
-Extned the SplittablesBase interface to OrderedCollections
+
+Extend the SplittablesBase interface to OrderedCollections
+
+This is a lightweight package that adds method definitions for 
+`amount` and `halve` to `OrderedDict` and `OrderedSet` and `LittleDict`.
+
+Add the package with:
+
+```julia
+] add https://github.com/April-Hannah-Lena/SplitOrderedCollections.jl.git
+```
+
+Use the package with:
+
+```julia
+using OrderedCollections
+using SplittablesBase
+using SplitOrderedCollections
+```
+
+The package conforms with the SplittablesBase Interface. In particular, `halve` for 
+`OrderedDict`, `OrderedSet`, `LittleDict` should return the same type as `halve` for
+regular `Dict` and `Set` so that code can be general without type instabilities.
---
+++ src/SplitOrderedCollections.jl
@@ -0,0 +1,30 @@
+using OrderedCollections
+using SplittablesBase
+
+_testrehash!(xs::OrderedDict) = xs.ndel > 0      && OrderedCollections.rehash!(xs)
+_testrehash!(xs::OrderedSet)  = xs.dict.ndel > 0 && OrderedCollections.rehash!(xs.dict)
+_testrehash!(xs::LittleDict)  = nothing
+
+SplittablesBase.DictView(xs::OrderedDict, i::Integer, j::Integer) = SplittablesBase.DictView(xs,      i, j, identity)
+SplittablesBase.DictView(xs::LittleDict,  i::Integer, j::Integer) = SplittablesBase.DictView(xs,      i, j, identity)
+SplittablesBase.DictView(xs::Orderedset,  i::Integer, j::Integer) = SplittablesBase.DictView(xs.dict, i, j, first)
+
+SplittablesBase.firstslot(::Union{OrderedDict,OrderedSet,LittleDict}) = 1
+
+SplittablesBase.lastslot(xs::Union{OrderedDict,LittleDict}) = length(xs.keys)
+SplittablesBase.lastslot(xs::OrderedSet) = length(xs.dict.keys)
+
+function SplittablesBase.amount(xs::Union{OrderedDict,OrderedSet,LittleDict})
+    _testrehash!(xs)
+    return SplittablesBase.lastslot(xs)# - SplittablesBase.firstslot(xs) + 1
+end
+
+function SplittablesBase.halve(xs::Union{OrderedDict,OrderedSet,LittleDict})
+    _testrehash!(xs)
+    i1 = SplittablesBase.firstslot(xs)
+    i3 = SplittablesBase.lastslot(xs)
+    i2 = i3 ÷ 2
+    left = SplittablesBase.DictView(xs, i1, i2)
+    right = SplittablesBase.DictView(xs, i2 + 1, i3)
+    return (left, right)
+end
