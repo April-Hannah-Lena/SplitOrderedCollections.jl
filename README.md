99451f8992655b08f173e89e13dea5a1a16dbe5b
April-Hannah-Lena <aprillherwig@gmail.com>
2022-07-17 16:26:55 +0200

    add BitSet definitions

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Extend the SplittablesBase interface to OrderedCollections
 
 This is a lightweight package that adds method definitions for 
-`amount` and `halve` to `OrderedDict` and `OrderedSet` and `LittleDict`.
+`amount` and `halve` to `OrderedDict`, `OrderedSet`, `LittleDict`.
+There is also a method for `BitSet` from Julia Base.
 
 Add the package with:
 
@@ -19,6 +20,6 @@ using SplittablesBase
 using SplitOrderedCollections
 ```
 
-The package conforms with the SplittablesBase Interface. In particular, `halve` for 
-`OrderedDict`, `OrderedSet`, `LittleDict` should return the same type as `halve` for
-regular `Dict` and `Set` so that code can be general without type instabilities.
+The package conforms with the SplittablesBase Interface. In particular, 
+the methods defined here return the same type as `amount` and `halve` for
+regular `Dict` and `Set` so that code can be general without type instabilities. 
--- src/SplitOrderedCollections.jl
+++ src/SplitOrderedCollections.jl
@@ -4,10 +4,25 @@ using OrderedCollections
 using SplittablesBase
 import SplittablesBase: amount, halve
 import SplittablesBase.Implementations: firstslot, lastslot, DictView
+import Base: iterate
+
+# common
+
+amount(xs::Union{OrderedDict,OrderedSet,LittleDict,BitSet}) = lastslot(xs)
+
+function halve(xs::Union{OrderedDict,OrderedSet,LittleDict,BitSet})
+    i1 = firstslot(xs)
+    i3 = lastslot(xs)
+    i2 = i3 ÷ 2
+    left = DictView(xs, i1, i2)
+    right = DictView(xs, i2 + 1, i3)
+    return (left, right)
+end
+
+# interface for `OrderedCollections`
 
 _testrehash!(xs::OrderedDict) = xs.ndel > 0      && OrderedCollections.rehash!(xs)
 _testrehash!(xs::OrderedSet)  = xs.dict.ndel > 0 && OrderedCollections.rehash!(xs.dict)
-_testrehash!(xs::LittleDict)  = nothing
 
 DictView(xs::OrderedDict, i::Int, j::Int) = DictView(xs,      i, j, identity)
 DictView(xs::LittleDict,  i::Int, j::Int) = DictView(xs,      i, j, identity)
@@ -15,22 +30,25 @@ DictView(xs::OrderedSet,  i::Int, j::Int) = DictView(xs.dict, i, j, first)
 
 firstslot(::Union{OrderedDict,OrderedSet,LittleDict}) = 1
 
-lastslot(xs::Union{OrderedDict,LittleDict}) = length(xs.keys)
-lastslot(xs::OrderedSet) = length(xs.dict.keys)
+lastslot(xs::OrderedDict) = (_testrehash!(xs); length(xs.keys))
+lastslot(xs::OrderedSet)  = (_testrehash!(xs); length(xs.dict.keys))
+lastslot(xs::LittleDict)  = length(xs.keys)
 
-function amount(xs::Union{OrderedDict,OrderedSet,LittleDict})
-    _testrehash!(xs)
-    return lastslot(xs)# - SplittablesBase.firstslot(xs) + 1
-end
+# interface for `BitSet`
 
-function halve(xs::Union{OrderedDict,OrderedSet,LittleDict})
-    _testrehash!(xs)
-    i1 = firstslot(xs)
-    i3 = lastslot(xs)
-    i2 = i3 ÷ 2
-    left = DictView(xs, i1, i2)
-    right = DictView(xs, i2 + 1, i3)
-    return (left, right)
+DictView(xs::BitSet, i::Int, j::Int) = DictView(xs, i, j, identity)
+
+firstslot(xs::BitSet) = 1
+
+lastslot(xs::BitSet) = length(xs.bits)
+
+function iterate(xs::DictView{BitSet}, i = (Base.CHK0, xs.firstslot - 1))
+    last(i) <= xs.lastslot || return nothing
+    y = iterate(xs.dict, i)
+    y === nothing && return nothing
+    x, j = y
+    last(j) <= xs.lastslot + 1 && return xs.f(x), j
+    return nothing
 end
 
 end # module
